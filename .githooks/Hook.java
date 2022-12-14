2f8a7ab66cce848c441541ce994284305003da82
MoussironNathan <nathan.moussiron@free.fr>
2022-12-14 18:36:44 +0100

    pre-push hook

--- .githooks/Hook.java
+++ .githooks/Hook.java
@@ -1,7 +1,11 @@
 public class Hook{
 
     public static void main(String[] args) {
-        System.out.println("Bonjour");
-        System.exit(0);
+        if("test".equals(args[1])){
+            System.out.println("Bonjour " + args[0]);
+            System.exit(0);
+        }
+        System.out.println(args[0]);
+        System.exit(1);
     }
 }
